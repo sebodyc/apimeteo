dafa3c32e272a6dacb9d06ff8aca60cec6d5ebc6
seb <damore.sebastien@gmail.com>
2020-03-19 13:44:48 +0100

    ajout de la requette a l'api en recherchant par ville

--- index.php
+++ index.php
@@ -16,9 +16,23 @@
 <input type="text" id="change" name="change" required>
 </form>
 
+<?php
+
+$search = $_POST["change"];
+
+$token_api= "1c4537151018f8f0fbdd98c153dbe4c7f1d69f6ecab72ddee86a5fcd6f70c54c";
+$data = file_get_contents('https://api.meteo-concept.com/api/location/cities?token='.$token_api.'&search='.$search);
+$cities = json_decode($data);
+echo '<pre>';
+print_r($cities);
+echo '</pre>';
 
 
 
-    
+
+ ?>
+
+
+
   </body>
 </html>
