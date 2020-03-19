fc08dcc125c524f296e79a46923563b1bfbf85bf
seb <damore.sebastien@gmail.com>
2020-03-19 13:31:05 +0100

    ajout dun formulaire pour pouvoir tester l'api plus facilement

---
+++ index.php
@@ -0,0 +1,24 @@
+<html  lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>mon air.fr</title>
+    <link rel="stylesheet" href="style.css">
+
+
+
+  </head>
+  <body>
+
+      <form class="" action="index.php" method="post">
+
+    <label for="name">ville</label>
+
+<input type="text" id="change" name="change" required>
+</form>
+
+
+
+
+    
+  </body>
+</html>
